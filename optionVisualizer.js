a9a76f6052f9062f323820088c2f5186f1c40fb3
TomJ <trjcgda@yahoo.com>
2022-03-10 14:01:35 -0800

    added blank line

--- optionVisualizer.js
+++ optionVisualizer.js
@@ -77,6 +77,7 @@ function init() {
     //document.write(getOptionValue("put", 100, .1, 84, vol, rfr));
 
 
+    
     //document.write('a');
     var table = getGridNumbers("put", 100, 1, 100, vol, rfr);
     //document.write('a2');
